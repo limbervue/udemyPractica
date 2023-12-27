0d2d7cd40db3f688e7d57b919fa52c0b83b7ec8a
limbervue <limbervue9596@gmail.com>
2023-12-27 16:49:24 -0500

    udemy_prueba

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(".imagen_b").bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 800
+      });
+
+      $(".imagen_c").bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 800
+      });
+})
